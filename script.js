e1c0f4ec8d04b19d7412daea33f8e1a8e144ecd7
Elizabeth dos Santos <62735673+elizabethesantos@users.noreply.github.com>
2022-11-21 22:14:43 +0000

    responsividade e menu

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const hamburger = document.querySelector(".hamburger");
+const hamburgerIcone = document.querySelector(".hamburgerIcone");
 const navMenu = document.querySelector(".menuSection");
 const menu = document.querySelector('.menu');
+const baseMenu = document.querySelector('.baseMenu')
 const exit = document.querySelector('.exit');
 
 hamburger.addEventListener("click", () => {
-  //menu.style.display = 'initial'
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+  menu.classList.add('flex')
+  menu.classList.add('menuSection')
+  hamburgerIcone.classList.add("invisivel");
+  navMenu.classList.add("flex");
+  navMenu.classList.add("active");
 });
 
-document.querySelectorAll(".navegacaoLinks").forEach((link) =>
+exit.addEventListener('click', () => {
+  menu.classList.remove('flex')
+  menu.classList.remove('menuSection')
+  hamburgerIcone.classList.remove("invisivel");
+  navMenu.classList.add("active");
+})
+
+/* document.querySelectorAll(".navegacaoLinks").forEach((link) =>
   link.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   })
-);
+); */
 
 
